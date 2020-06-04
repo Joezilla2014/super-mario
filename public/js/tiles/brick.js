4a7aa47c005bb836f13dd34e79bd891fbcba8ad2
Pontus Alexander <pontus.alexander@gmail.com>
2020-06-04 19:50:23 +0200

    Clean code

--- public/js/tiles/brick.js
+++ public/js/tiles/brick.js
@@ -1,32 +1,40 @@
 import { Vec2 } from '../math.js';
-import {Sides} from '../Entity.js';
+import { Sides } from '../Entity.js';
 import Player from "../traits/Player.js";
 
-function createSharpnel(gameContext, match) {
-    const center = new Vec2(
+function centerEntity(entity, pos) {
+    entity.pos.x = pos.x - entity.size.x / 2;
+    entity.pos.y = pos.y - entity.size.y / 2;
+}
+
+function getMatchCenter(match) {
+    return new Vec2(
         match.x1 + ((match.x2 - match.x1) / 2),
         match.y1 + ((match.y2 - match.y1) / 2),
     );
+}
 
-    const bricks = [
-        gameContext.entityFactory.brickSharpnel(),
-        gameContext.entityFactory.brickSharpnel(),
-        gameContext.entityFactory.brickSharpnel(),
-        gameContext.entityFactory.brickSharpnel(),
-    ];
-    for (const brick of bricks) {
-        brick.pos.copy(center);
+function addShrapnel(level, gameContext, match) {
+    const center = getMatchCenter(match);
+
+    const bricks = [];
+    for (let i = 0; i < 4; i++) {
+        const brick = gameContext.entityFactory.brickSharpnel();
+        centerEntity(brick, center);
+        level.entities.add(brick);
+        bricks.push(brick);
     }
+
+    const spreadH = 60;
+    const spreadV = 400;
     bricks[0].sounds.add('break');
-    bricks[0].vel.set(-50, -400);
-    bricks[1].vel.set(-25, -400);
-    bricks[2].vel.set(25, -400);
-    bricks[3].vel.set(50, -400);
-    return bricks;
+    bricks[0].vel.set(-spreadH, -spreadV * 1.2);
+    bricks[1].vel.set(-spreadH, -spreadV);
+    bricks[2].vel.set(spreadH, -spreadV * 1.2);
+    bricks[3].vel.set(spreadH, -spreadV);
 }
 
 function handleX({entity, match}) {
-
     if (entity.vel.x > 0) {
         if (entity.bounds.right > match.x1) {
             entity.obstruct(Sides.RIGHT, match);
@@ -47,11 +55,7 @@ function handleY({entity, match, resolver, gameContext, level}) {
         if (entity.traits.has(Player)) {
             const grid = resolver.matrix;
             grid.delete(match.indexX, match.indexY);
-
-            const shrapnel = createSharpnel(gameContext, match);
-            for (const entity of shrapnel) {
-                level.entities.add(entity);
-            }
+            addShrapnel(level, gameContext, match);
         }
 
         if (entity.bounds.top < match.y2) {
